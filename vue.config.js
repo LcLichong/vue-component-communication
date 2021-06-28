99cfe2709a83a960b3b4f4070c68ddba6a50918e
LcLichong <736117210@qq.com>
2021-06-28 21:20:20 +0800

    add dist

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-    productionSourceMap: false,
-    publicPath: './',
+    productionSourceMap: process.env.NODE_ENV !== 'production',
+    publicPath: '/',
     devServer: {
         open: true,
         hotOnly: true
-    }
+    },
+    chainWebpack: config => {
+        config.plugin('html').tap(options => {
+            options[0].title = 'Vue组件通信'
+            return options
+        })
+    },
 }
