94de4afdf91c61ffd9e158a502ff88e90e58efe0
LcLichong <736117210@qq.com>
2021-06-29 14:26:57 +0800

    打包

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+    // 生产环境去掉console
+    prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ],
+    plugins: [
+        ...prodPlugins
+    ]
 }
